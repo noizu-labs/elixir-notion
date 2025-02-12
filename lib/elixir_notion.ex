23878117ab33b2aa7d74e69e2544136d01dfabe7
noizu <keith.brings@noizu.com>
2025-02-13 02:15:30 +0700

    initial

---
+++ lib/elixir_notion.ex
@@ -0,0 +1,18 @@
+defmodule ElixirNotion do
+  @moduledoc """
+  Documentation for `ElixirNotion`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> ElixirNotion.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/elixir_notion_test.exs
@@ -0,0 +1,8 @@
+defmodule ElixirNotionTest do
+  use ExUnit.Case
+  doctest ElixirNotion
+
+  test "greets the world" do
+    assert ElixirNotion.hello() == :world
+  end
+end
